ffb2d062104a0bc46baa0a13efd093805325f669
David Blankenship <david.blankenship@yale.edu>
2023-05-15 16:45:45 -0400

    Make Twig/Drupal and storybook work together

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,21 @@
+import { useEffect } from '@storybook/client-api';
 import Twig from 'twig';
 import { setupTwig } from './setupTwig';
 import '@yalesites-org/component-library-twig/dist/style.css';
+
+import './_drupal.js';
 
 setupTwig(Twig);
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    (Story) => {
+      const searchParams = new URLSearchParams(window.location.search);
+      useEffect(() => Drupal.attachBehaviors(), [searchParams]);
+      return (<Story />);
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
